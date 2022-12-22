bab008223f59f72e03516ea15f487a98f8ddbdc9
francisamidu <francisamidu124@gmail.com>
2022-12-22 11:11:30 +0200

    Update

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native-ui-lib'
+import 'react-native-gesture-handler'
+
+export default () => {
+  return (
+    <View paddingH-25 paddingT-120>
+      <Text text20>This is React Native UI Lib</Text>
+    </View>
+  )
+}
